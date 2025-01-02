f45bca038882ca156c9678ce7076ae8797901871
nquidox <nquidox@gmail.com>
2025-01-02 16:03:57 +0300

    extended

--- config/config.go
+++ config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "os"
+import (
+	log "github.com/sirupsen/logrus"
+	"os"
+	"strconv"
+)
 
 type Config struct {
 	AppConfig  AppConfig
@@ -11,6 +15,7 @@ type AppConfig struct {
 	Host     string
 	Port     string
 	LogLevel string
+	NumCPUs  int
 }
 
 type RepoConfig struct {
@@ -23,7 +28,8 @@ func NewConfig() *Config {
 		AppConfig: AppConfig{
 			Host:     getEnv("APP_HOST", "0.0.0.0"),
 			Port:     getEnv("APP_PORT", "9050"),
-			LogLevel: getEnv("APP_LOG_LEVEL", "info"),
+			LogLevel: getEnv("APP_LOG_LEVEL", "debug"),
+			NumCPUs:  getEnvInt("APP_NUMCPUs", -1),
 		},
 		RepoConfig: RepoConfig{
 			Host: getEnv("REPO_HOST", "0.0.0.0"),
@@ -35,6 +41,18 @@ func NewConfig() *Config {
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
+	}
+	return fallback
+}
+
+func getEnvInt(key string, fallback int) int {
+	if value, ok := os.LookupEnv(key); ok {
+		num, err := strconv.Atoi(value)
+		if err != nil {
+			log.WithField("default", -1).Warn("Config | Can't parse value as int")
+			return fallback
+		}
+		return num
 	}
 	return fallback
 }

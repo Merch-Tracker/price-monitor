78109dddf22782384cd8b459113e4f9917984ec9
nquidox <nquidox@gmail.com>
2025-01-09 18:36:53 +0300

    extended: parsing + debug logs

--- internal/parser/worker.go
+++ internal/parser/worker.go
@@ -1,29 +1,45 @@
 package parser
 
 import (
+	log "github.com/sirupsen/logrus"
 	"parsing-service/internal/network"
 )
 
 func ProcessData(in <-chan network.Merch, out chan<- network.MerchResp) {
 	for job := range in {
+		log.Debugf("Processing job: %s", job)
+
+		log.WithFields(log.Fields{
+			"link":          job.Link,
+			"cookie values": job.CookieValues,
+		}).Debug("Parsing | Request data")
+
 		page, err := getPage(job.Link, job.CookieValues)
 		if err != nil {
-			return
+			log.WithField("err", err).Debug("Parsing | Can't get page")
+			break
 		}
+		log.WithField("Page fetched", page != nil).Debug("Parsing | Step 1")
 
-		results := findData(page, job.ParseTag, job.ParseSubstring)
-		if results == nil {
-			return
+		var data []string
+
+		symbols := makeSymbolsList(job.ParseSubstring)
+		for _, symbol := range symbols {
+			data = append(data, findData(page, job.ParseTag, symbol)...)
 		}
+		log.WithField("find data", len(data)).Debug("Parsing | Step 2")
 
 		var price uint32 = 0
 
-		if len(results) > 0 {
-			price = uint32(findMinimal(results, job.Separator))
+		if len(data) > 0 {
+			price = uint32(findMinimal(data, job.Separator))
 		}
+		log.WithField("find minimal price", price).Debug("Parsing | Step 3")
 
 		if price > 0 {
 			out <- network.MerchResp{MerchUuid: job.MerchUuid, Price: price}
 		}
+		log.WithField("price", price).Debug("Parsing | Step 4 END")
 	}
+	log.Error("Data processing aborted")
 }

e51a8fad8508063313a1d05403ccb7538655b2ba
nquidox <nquidox@gmail.com>
2025-01-09 18:37:28 +0300

    extended: debug logs

--- internal/parser/getPage.go
+++ internal/parser/getPage.go
@@ -1,33 +1,37 @@
 package parser
 
 import (
+	log "github.com/sirupsen/logrus"
 	"golang.org/x/net/html"
-	"log"
 	"net/http"
 	"os"
 )
 
 func getPage(url, cookieValues string) (*html.Node, error) {
+	log.WithField("url length", len(url)).Debug("Fetching page")
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
+		log.WithField("err", err).Error("Get Page | request")
 		return nil, err
 	}
 	req.Header.Set("Cookie", cookieValues)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
-
 	if err != nil {
+		log.WithField("err", err).Error("Get Page | response")
 		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
+		log.WithField("code", resp.StatusCode).Error("Get Page | response status != 200")
 		return nil, err
 	}
 
 	doc, err := html.Parse(resp.Body)
 	if err != nil {
+		log.WithField("err", err).Error("Get Page | html parse")
 		return nil, err
 	}
 	return doc, nil
